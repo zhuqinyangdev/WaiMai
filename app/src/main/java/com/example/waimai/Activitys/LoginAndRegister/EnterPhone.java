c7b0d7630a086939be1a2c2db9361d0258e7bfca
ZQY <320338208@qq.com>
2017-11-20 18:30:10 +0800

    手机号输入界面完成

---
+++ app/src/main/java/com/example/waimai/Activitys/LoginAndRegister/EnterPhone.java
@@ -0,0 +1,15 @@
+package com.example.waimai.Activitys.LoginAndRegister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.waimai.R;
+
+public class EnterPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_phone_number_layout);
+    }
+}
--- app/src/main/java/com/example/waimai/LoginActivity.java
+++ app/src/main/java/com/example/waimai/LoginActivity.java
@@ -1,13 +1,50 @@
 package com.example.waimai;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.waimai.Activitys.LoginAndRegister.EnterPhone;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView register; //注册新用户
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        init();
+        bindOnClick();
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void init(){
+        register = (TextView)findViewById(R.id.register);
+    }
+
+    /**
+     * 按钮单机事件绑定
+     */
+    private void bindOnClick(){
+        register.setOnClickListener(this);
+    }
+
+    /**
+     * 控件点击事件
+     * @param view
+     */
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.register:
+                Intent intent = new Intent(this, EnterPhone.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
